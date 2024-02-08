29e974171a39d82eefb82eea350f0e62ea5ebdb8
Jivak Sute <jivakysute@gmail.com>
2024-02-08 13:41:45 +0530

    hello name created

---
+++ nextapp/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function index() {
+  return (
+    <div>
+      <h1>Hello from,</h1>
+      <h1>
+        <b>
+          <i>Jivak Sute</i>
+        </b>
+      </h1>
+    </div>
+  );
+}
+
+export default index;

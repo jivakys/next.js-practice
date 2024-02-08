49bd7d97af9e1e6af50e8be51aa12b798830fe85
Jivak Sute <jivakysute@gmail.com>
2024-02-08 14:28:24 +0530

    created new file-pages

--- nextapp/pages/index.jsx
+++ nextapp/pages/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function index() {
   return (
-    <div>
+    <>
       <h1>Hello from,</h1>
       <h1>
         <b>
           <i>Jivak Sute</i>
         </b>
       </h1>
-    </div>
+    </>
   );
 }
 
